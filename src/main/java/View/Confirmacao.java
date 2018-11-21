0352bbf68342796b03785766b76eb07b6a7bf875
GabsFaria <gabriel-farinha@hotmail.com>
2018-11-21 10:14:01 -0200

    Projeto 97% finalizado

--- src/main/java/View/Confirmacao.java
+++ src/main/java/View/Confirmacao.java
@@ -4,14 +4,18 @@ import Controller.CarroController;
 import Model.CarroEntity;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Confirmacao {
     private JFrame janelaConfirmacao;
+    private JTable table;
+    private DefaultTableModel modelo;
+    private JPanel panel = new JPanel();
     private JButton confirmar = new JButton("Confirmar");
     private JButton cancelar = new JButton("Cancelar");
-    private JLabel resultado;
 
     public void confirma(CarroEntity carroEntity) {
 
@@ -37,18 +41,36 @@ public class Confirmacao {
 
     public void criaJanela(final CarroEntity carroEntity){
         janelaConfirmacao = new JFrame("Confirmação");
-        janelaConfirmacao.setSize(470,150);
+        String[][] conteudo = new String[1][4];
+        String[] colunas = {"Nome", "Marca", "Ano", "Placa"};
+
+        conteudo[0][0] = carroEntity.getNome();
+        conteudo[0][1] = carroEntity.getMarca();
+        conteudo[0][2] = carroEntity.getAno();
+        conteudo[0][3] = carroEntity.getPlaca();
+
+        modelo = new DefaultTableModel(conteudo,colunas);
+
+        table = new JTable(modelo);
+        table.setBounds(10,10,520,30);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        panel.setSize(490,40);
+        janelaConfirmacao.setSize(500,150);
+        janelaConfirmacao.add(panel);
         janelaConfirmacao.setLayout(null);
         janelaConfirmacao.setLocationRelativeTo(null);
         janelaConfirmacao.setVisible(true);
         janelaConfirmacao.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        String carro = carroEntity.toString();
-        resultado = new JLabel(carro);
-        resultado.setBounds(10,10,460,25);
-        janelaConfirmacao.add(resultado);
         confirmar.setBounds(230,60,100,40);
         confirmar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                carroEntity.setNome((String) table.getValueAt(0,0));
+                carroEntity.setMarca((String) table.getValueAt(0,1));
+                carroEntity.setAno((String) table.getValueAt(0,2));
+                carroEntity.setPlaca((String) table.getValueAt(0,3));
                 CarroController carroController = new CarroController();
                 carroController.inserir(carroEntity);
                 janelaConfirmacao.setVisible(false);
