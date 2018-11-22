71ef0a84d56c46cfe89b49a2c0446b9900647ba8
Gabriel <gabriel-farinha2011@hotmail.com>
2018-11-22 17:30:18 -0200

    Projeto 99% finalizado

--- src/main/java/View/Teste.java
+++ src/main/java/View/Teste.java
@@ -4,35 +4,6 @@ public class Teste {
     public static void main(String[] args) {
           TextoPainelPrincipal text = new TextoPainelPrincipal();
           text.inserirTexto();
-//        CarroEntity carroDAO = new CarroEntity();
-//        carroDAO.setNome(JOptionPane.showInputDialog("Digite o nome do carro"));
-//        carroDAO.setAno(JOptionPane.showInputDialog("Digite o ano do carro"));
-//        carroDAO.setMarca(JOptionPane.showInputDialog("Digite a marca do carro"));
-//        carroDAO.setPlaca(JOptionPane.showInputDialog("Digite a placa do carro"));
-//
-//        CarroEntity carroDAO1 = new CarroEntity();
-//        carroDAO1.setNome(JOptionPane.showInputDialog("Digite o nome do carro"));
-//        carroDAO1.setAno(JOptionPane.showInputDialog("Digite o ano do carro"));
-//        carroDAO1.setMarca(JOptionPane.showInputDialog("Digite a marca do carro"));
-//        carroDAO1.setPlaca(JOptionPane.showInputDialog("Digite a placa do carro"));
-//
-//        CarroController carro = new CarroController();
-//        carro.inserir(carroDAO1);
-//        List<CarroEntity> list = carro.listar();
-//
-//        for (int i = 0; i<list.size(); i++) {
-//            JOptionPane.showMessageDialog(null,"Carro: "+list.get(i).getNome()+", Placa: "+list.get(i).getPlaca());
-//        }
-
-//        CarroEntity carroDAO = new CarroEntity();
-//        carroDAO.setId(Integer.parseInt(JOptionPane.showInputDialog("Digite o Id que deseja alterar")));
-//        carroDAO.setNome(JOptionPane.showInputDialog("Digite o nome do carro"));
-//        carroDAO.setAno(JOptionPane.showInputDialog("Digite o ano do carro"));
-//        carroDAO.setMarca(JOptionPane.showInputDialog("Digite a marca do carro"));
-//        carroDAO .setPlaca(JOptionPane.showInputDialog("Digite a placa do carro"));
-//
-//        carro.atualizar(carroDAO);
-
     }
 
 
